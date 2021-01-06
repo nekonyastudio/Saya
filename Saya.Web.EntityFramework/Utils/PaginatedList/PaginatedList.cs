f812d7a43b8f48b26c10b3c3d9f8bfe8329ec586
大魔王有木桑 <yomunsam@outlook.com>
2021-01-06 11:48:23 +0800

    hello world

---
+++ Saya.Web.EntityFramework/Utils/PaginatedList/PaginatedList.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Saya.Web.Utils.PaginatedList
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+
+        public int ItemCount { get; set; }
+
+        public PaginatedList(IList<T> items, int count, int pageIndex, int pageSize)
+        {
+            this.PageIndex = pageIndex;
+            this.TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+
+            this.AddRange(items);
+
+            this.ItemCount = count;
+        }
+
+        /// <summary>
+        /// 创建分页数据
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = await source.CountAsync();
+            var items = await source
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+
+    }
+}
---
+++ Saya.Web/Dto/PaginatedList/PaginatedListDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace Saya.Web.Dto.PaginatedList
+{
+    public class PaginatedListDto<T>
+    {
+        /// <summary>
+        /// 当前页 索引序号
+        /// </summary>
+        [JsonPropertyName("index")]
+        public int PageIndex { get; set; }
+
+        /// <summary>
+        /// 总页数
+        /// </summary>
+        [JsonPropertyName("total")]
+        public int TotalPages { get; set; }
+
+        /// <summary>
+        /// 总数据数
+        /// </summary>
+        [JsonPropertyName("count")]
+        public long ItemCount { get; set; }
+
+        /// <summary>
+        /// 最终数据
+        /// </summary>
+        [JsonPropertyName("data")]
+        public IEnumerable<T> Data { get; set; }
+    }
+}
